161d5a2bf15d0c053f51d83127a06eb13fc9a84e
DenisChpt <denis.chaput@pm.me>
2025-04-07 15:22:16 +0200

    Add IconTwitter component and update icons index export

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,5 +3,6 @@ import IconEmail from './IconEmail.vue'
 import IconGithub from './IconGithub.vue'
 import IconLinkedin from './IconLinkedin.vue'
 import IconLocation from './IconLocation.vue'
+import IconTwitter from './IconTwitter.vue'
 
-export { IconBase, IconEmail, IconGithub, IconLinkedin, IconLocation }
+export { IconBase, IconEmail, IconGithub, IconLinkedin, IconLocation, IconTwitter }
